626dd683d4e43a06a5267ad2502ff7f504ea7de9
Stijn van de Sande <stijnvandesande@ziggo.nl>
2021-10-25 16:30:01 +0200

    Update selector

--- src/app/dynamic-table/dynamic-table.component.ts
+++ src/app/dynamic-table/dynamic-table.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-dynamic-table',
+  selector: 'dynamic-table',
   templateUrl: './dynamic-table.component.html',
   styleUrls: ['./dynamic-table.component.scss']
 })
