cde524ebade60b347975683f877ed1ef7b82a9e6
Stijn van de Sande <stijnvandesande@ziggo.nl>
2021-10-25 16:18:39 +0200

    Create empty generic table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicTableComponent
   ],
   imports: [
     BrowserModule,
